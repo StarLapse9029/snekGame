f77ebea0bd4112ab0a610f5ad2c060640f12889b
StarLapse9029 <49539749+StarLapse9029@users.noreply.github.com>
2024-10-29 23:37:16 -0300

    .

--- c/main.c
+++ c/main.c
@@ -18,23 +18,45 @@ typedef struct{
   struct Node * next;
 } Node;
 
+typedef struct{
+  int x;
+  int y;
+}Direction;
+
+typedef struct{
+  int x;
+  int y;
+} Point;
+
 // Constants
 int WINDOW_HEIGHT = 800;
 int WINDOW_WIDTH = 1200;
 int SEG_SIZE = 50;
-int DELAY = 100;
+int DELAY = 10;
 
 // Function Declarations
-bool events();
+bool events(Direction *dir);
 SDL_Window* initWindow(int x, int y);
 SDL_Renderer* initRenderer(SDL_Window * window);
 void drawSquare(int x, int y, SDL_Renderer * renderer);
 Node *initSnake();
 void *freeSnake(Node *snake);
 void *drawSnake(Node *snake, SDL_Renderer * renderer);
-void drawFruit(SDL_Renderer * renderer);
+int drawFruit(Point a, SDL_Renderer * renderer);
+void moveSnake(Node * snake, Direction *dir);
+void clearRender(SDL_Renderer * renderer);
+Point fruitLocation();
+void addSegment(Node * snake);
+int eat(Node * snake, Point fruit);
+void hitWall(Node * snake);
+
 // Main 
 int main(void){
+  Direction dir;
+  dir.x = 1;
+  dir.y = 0;
+  Point a = fruitLocation();
+  int fruits = 0;
   srand(time(NULL));
   bool runnning = true;
   Node* snake = initSnake();
@@ -54,9 +76,16 @@ printf("Could not initialize SDL: %s\n", SDL_GetError());
   SDL_RenderPresent(renderer);
 
   while(runnning){
-    runnning = events();
+    runnning = events(&dir);
+    clearRender(renderer);
+    hitWall(snake);
+    fruits = eat(snake, a);
+    moveSnake(snake, &dir);
     drawSnake(snake, renderer);
-    drawFruit(renderer);
+    if(fruits == 0){
+      a = fruitLocation();
+    }
+    drawFruit(a, renderer);
     SDL_RenderPresent(renderer);
     SDL_Delay(DELAY);
   }
@@ -69,7 +98,7 @@ printf("Could not initialize SDL: %s\n", SDL_GetError());
   return 0;
 }
 
-bool events(){
+bool events(Direction *dir){
   SDL_Event event;
       while(SDL_PollEvent(&event)){
         switch (event.type) {
@@ -79,6 +108,22 @@ bool events(){
               switch(event.key.keysym.sym){
                 case SDLK_ESCAPE:
                   return false;
+                case SDLK_a:
+                  dir->x = -1;
+                  dir->y = 0;
+                  break;
+                case SDLK_s:
+                  dir->x = 0;
+                  dir->y = 1;
+                  break;
+                case SDLK_d:
+                  dir->x = 1;
+                  dir->y = 0;
+                  break;
+                case SDLK_w:
+                  dir->x = 0;
+                  dir->y = -1;
+                  break;
                 default:
                   break;
             }
@@ -121,6 +166,12 @@ void drawSquare(int x, int y, SDL_Renderer * renderer){
   SDL_RenderFillRect(renderer, &rect);
 }
 
+void clearRender(SDL_Renderer * renderer){
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+}
+
+// Snake
 Node *initSnake(){
   Node *head = (Node*)malloc(sizeof(Node));
   head->xcor = WINDOW_WIDTH/2;
@@ -139,19 +190,77 @@ void *freeSnake(Node *snake){
   return 0;
 }
 
-void *drawSnake(Node * snake, SDL_Renderer * renderer){
-  Node *tmp = snake;
-  while(tmp){
-    drawSquare(tmp->xcor, tmp->ycor, renderer);
-    tmp = (Node*)tmp->next;
-  }
+void *drawSnake(Node *snake, SDL_Renderer *renderer) {
+    Node *tmp = snake;
+    while (tmp != NULL) {
+        drawSquare(tmp->xcor, tmp->ycor, renderer);
+        tmp = (Node*)tmp->next;
+    }
   return 0;
 }
 
-void drawFruit(SDL_Renderer * renderer){
-  int x = rand() % 1175 + 26;
-  int y = rand() % 775 + 26;
+
+void moveSnake(Node * snake, Direction *dir){
+  Node * tmp = snake;
+  while(tmp){
+    tmp->xcor += dir->x*10;
+    tmp->ycor += dir->y*10;
+    tmp = (Node*)tmp->next;
+  }
+}
+
+void addSegment(Node * snake){
+  Node * tmp = snake;
+  Node * new = (Node*)malloc(sizeof(Node));
+  if(new == NULL){
+    return;
+  }
+  while(tmp->next != NULL){
+    tmp = (Node*)tmp->next;
+  }
+  new->next = NULL;
+  new->xcor = tmp->xcor;
+  new->ycor = tmp->ycor;
+  tmp->next = (struct Node*)new;
+  puts("hi");
+}
+// Collisions
+int eat(Node * snake, Point fruit){
+  if(snake->xcor >= fruit.x - 15 && snake->xcor <= fruit.x + 15){
+    if (snake->ycor >= fruit.y - 15 && snake->ycor <= fruit.y + 15) {
+      addSegment(snake);
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void hitWall(Node * snake){
+  if(snake->xcor >= WINDOW_WIDTH){
+    snake->xcor = 0;
+  }else if(snake->xcor <= 0){
+    snake->xcor = WINDOW_WIDTH;
+  }else if(snake->ycor >= WINDOW_HEIGHT){
+    snake->ycor = 0;
+  }else if(snake->ycor <= 0){
+    snake->ycor = WINDOW_HEIGHT;
+  }
+}
+
+// Fruit
+
+Point fruitLocation(){
+  Point a;
+  a.x = rand() % (WINDOW_WIDTH - (SEG_SIZE/2)) + SEG_SIZE/2 +1;
+  a.y = rand() % (WINDOW_HEIGHT - (SEG_SIZE/2) + SEG_SIZE/2 + 1);
+  return a;
+}
+
+int drawFruit(Point a, SDL_Renderer * renderer){
+  int x = a.x;
+  int y = a.y;  
   SDL_Rect fruit = {x, y, (int)(SEG_SIZE * 2/3), (int)(SEG_SIZE * 2/3)};
   SDL_SetRenderDrawColor(renderer, 255, 15, 15, 255);
   SDL_RenderFillRect(renderer, &fruit);
+  return 1;
 }
