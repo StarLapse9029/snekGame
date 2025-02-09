05a8581c75cc8f9e31eea655c31cfca166badb4b
StarLapse9029 <49539749+StarLapse9029@users.noreply.github.com>
2025-02-09 18:07:55 -0300

    testing objects in c

---
+++ c/new.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct{
+  int x;
+  int y;
+} coord;
+
+typedef struct Segment{
+  coord *posxy ;
+  struct Segment *next;
+  void (*snek)(struct Segment *self);
+} snake_segment;
+
+void snek(snake_segment *self){
+  printf("pos x: %d  -  pos y: %d\n", self->posxy->x, self->posxy->y);
+}
+
+snake_segment *snake_constructor(coord *current_position){
+  snake_segment *segment = malloc(sizeof(snake_segment));
+  if(segment == NULL) return 0;
+  segment->next = NULL;
+  coord position = {current_position->x, current_position->y};
+  segment->posxy = &position;
+  segment->snek = snek;
+  return segment;
+}
+coord current_position = {5, 10};
+
+int main(void){
+  snake_segment *a = snake_constructor(&current_position);
+  if(a == NULL) return 0;
+  a->snek(a);
+  free(a);
+  return 1;
+}
+
